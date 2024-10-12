358dbe8b67a0cf8b4bdbd7030a56713d8b7afa82
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-10-12 19:00:54 +0800

    Add use of uniforms for the color of the rectangle

--- src/Main.cpp
+++ src/Main.cpp
@@ -127,6 +127,8 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+	glfwSwapInterval(1);
+
     if (glewInit() != GLEW_OK)
         std::cout << "Failed to initialize GLEW" << std::endl;
 
@@ -164,16 +166,30 @@ int main(void)
     std::cout << source.FragmentSource << std::endl;
 
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-    glUseProgram(shader);
+    GLCall(glUseProgram(shader));
 
+    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+	ASSERT(location != -1);
+    GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
+
+    float r = 0.0f;
+    float increment = 0.5f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Draw a triangle */
+        /* Draw a rectangle */
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+			increment = 0.05f;
+
+		r += increment;
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
